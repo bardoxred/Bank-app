1033f4e0a964788e7950876a15eb1840e0659d6f
Konrad <wesolowskikonrad28@gmail.com>
2024-05-13 22:11:14 +0200

    Creating main frame layout

---
+++ Bank-desktop/src/forms/MainFrame.java
@@ -0,0 +1,57 @@
+package forms;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame {
+
+    private final int sizeX = 500;
+    private final int sizeY = 500;
+    private final String title = "Bank Application";
+
+    private JTextField emailField;
+    private JTextField passwordField;
+    private JPanel mainPanel;
+    private JLabel emailLabel;
+    private JLabel passwordLabel;
+    private JButton loginButton;
+
+    public MainFrame() {
+        this.setTitle(title); // tytuł
+        this.setSize(sizeX, sizeY); // wielkość okna
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // wyjscie po wyłączeniu
+        this.setVisible(true);  // wyświetlanie okienka
+        this.setLocationRelativeTo(null); // ustawienie okna na środku
+
+        // Panel główny
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(5, 1));
+        add(mainPanel);
+
+        emailLabel = new JLabel("Wpisz adres email:");
+        emailField = new JTextField();
+        mainPanel.add(emailLabel);
+        mainPanel.add(emailField);
+
+        passwordLabel = new JLabel("Hasło:");
+        passwordField = new JTextField();
+        mainPanel.add(passwordLabel);
+        mainPanel.add(passwordField);
+
+        loginButton = new JButton("Zaloguj się");
+        mainPanel.add(loginButton);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String emailAdress = emailField.getText();
+                String passwordAdress = passwordField.getText();
+
+                System.out.println(emailAdress);
+                System.out.println(passwordAdress);
+
+            }
+        });
+    }
+}
