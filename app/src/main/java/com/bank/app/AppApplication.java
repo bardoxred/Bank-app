b0a5b85b5d0a490e3db4a88eb06f19eac76e2b4a
Konrad <wesolowskikonrad28@gmail.com>
2024-05-24 10:44:10 +0200

    testing

--- app/src/main/java/com/bank/app/AppApplication.java
+++ app/src/main/java/com/bank/app/AppApplication.java
@@ -14,7 +14,6 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(AppApplication.class).headless(false).run(args);
-		System.out.println("blabla");
 	}
 	@Bean
 	public LoginFrame mainFrame(LoginFrameController loginFrameController, RegisterFrameController registerFrameController) {
